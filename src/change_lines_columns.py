80b0f160c07c466ac5d95364c5f83619a1292f35
ladcs <hpladcs@gmail.com>
2023-07-13 20:15:06 -0300

    feat: test change lines and rows and function

--- src/change_lines_columns.py
+++ src/change_lines_columns.py
@@ -1,4 +1,26 @@
-def change_first_line(particle, matrix):
-    particle_list = [particle] * len(matrix[0])
-    matrix[0] = particle_list
+def change_line(particle, matrix):
+    particle_first_line = [particle] * len(matrix[0])
+    matrix[0] = particle_first_line
+    if particle == 'p':
+        particle_last_line = particle_first_line
+        matrix[-1] = particle_last_line
     return matrix
+
+
+def change_column(particle, matrix):
+    particle_list = [particle] * len(matrix)
+    if particle == 'p':
+        for column in matrix:
+            column[-1] = particle_list[column[0]]
+            column[0] = particle_list[column[0]]
+        matrix[-2][-2] = 'p'
+        matrix[-1][-1] = 1
+        return matrix
+    for column in matrix:
+        column[-1] = particle_list[column[-1]]
+    return matrix
+
+
+if __name__ == '__main__':
+    print(change_column('p', [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1],
+                              [1, 1, 1, 1]]))
--- tests/test_change_lines_columns.py
+++ tests/test_change_lines_columns.py
@@ -1,21 +1,76 @@
-from src.change_lines_columns import change_first_line
+from src.change_lines_columns import change_line
+from src.change_lines_columns import change_column
 
 
 def test_first_line_matrix_4x4():
     matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
     matrix_result = [['e', 'e', 'e', 'e'], [1, 1, 1, 1], [1, 1, 1, 1],
                      [1, 1, 1, 1]]
-    assert change_first_line('e', matrix) == matrix_result
+    assert change_line('e', matrix) == matrix_result
 
 
 def test_first_line_matrix_4x3():
     matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
     matrix_result = [['e', 'e', 'e'], [1, 1, 1], [1, 1, 1],
                      [1, 1, 1]]
-    assert change_first_line('e', matrix) == matrix_result
+    assert change_line('e', matrix) == matrix_result
 
 
 def test_first_line_matrix_3x4():
     matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
     matrix_result = [['e', 'e', 'e', 'e'], [1, 1, 1, 1], [1, 1, 1, 1]]
-    assert change_first_line('e', matrix) == matrix_result
+    assert change_line('e', matrix) == matrix_result
+
+
+def test_line_with_p_matrix_4x4():
+    matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+    matrix_result = [['p', 'p', 'p', 'p'], [1, 1, 1, 1], [1, 1, 1, 1],
+                     ['p', 'p', 'p', 'p']]
+    assert change_line('p', matrix) == matrix_result
+
+
+def test_line_with_p_matrix_4x3():
+    matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    matrix_result = [['p', 'p', 'p'], [1, 1, 1], [1, 1, 1],
+                     ['p', 'p', 'p']]
+    assert change_line('p', matrix) == matrix_result
+
+
+def test_first_columns_matrix_4x4():
+    matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+    matrix_result = [[1, 1, 1, 'e'], [1, 1, 1, 'e'], [1, 1, 1, 'e'],
+                     [1, 1, 1, 'e']]
+    assert change_column('e', matrix) == matrix_result
+
+
+def test_first_columns_matrix_4x3():
+    matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    matrix_result = [[1, 1, 'e'], [1, 1, 'e'], [1, 1, 'e'],
+                     [1, 1, 'e']]
+    assert change_column('e', matrix) == matrix_result
+
+
+def test_first_columns_matrix_3x4():
+    matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+    matrix_result = [[1, 1, 1, 'e'], [1, 1, 1, 'e'], [1, 1, 1, 'e']]
+    assert change_column('e', matrix) == matrix_result
+
+
+def test_columns_with_p_matrix_4x4():
+    matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+    matrix_result = [['p', 1, 1, 'p'], ['p', 1, 1, 'p'], ['p', 1, 'p', 'p'],
+                     ['p', 1, 1, 1]]
+    assert change_column('p', matrix) == matrix_result
+
+
+def test_columns_with_p_matrix_4x3():
+    matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    matrix_result = [['p', 1, 'p'], ['p', 1, 'p'], ['p', 'p', 'p'],
+                     ['p', 1, 1]]
+    assert change_column('p', matrix) == matrix_result
+
+
+def test_columns_with_p_matrix_3x4():
+    matrix = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+    matrix_result = [['p', 1, 1, 'p'], ['p', 1, 'p', 'p'], ['p', 1, 1, 1]]
+    assert change_column('p', matrix) == matrix_result
